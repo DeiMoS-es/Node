e4849dd007f8627ca9dd4e16cd31a2de82f5fd54
DeiMoS <ndelgado@hiberus.com>
2023-07-25 11:20:09 +0200

    terminada clase 1

--- clase01/ej/9.http.js
+++ clase01/ej/9.http.js
@@ -1,16 +1,19 @@
 const http = require('node:http')
 const { findAvailablePort } = require('./10.free-port')
+console.log(process.env)
+const desiredPort = process.env.PORT ?? 3000 // Obtener el puerto pasandolo como parámetro en la ejecución del comando. Ej: PORT=1234 node .\9.http.js
 
 const server = http.createServer((req, res) => {
   console.log('request received')
   res.end('Hola Mundo')
 })
 
-findAvailablePort(1234).then(port => {
+findAvailablePort(desiredPort).then(port => {
   server.listen(port, () => {
     console.log(`server listening on port http://localhost:${port}`)
   })
 })
+// Para ejecutar esto en Windows set PORT=1234 ; node .\9.http.js
 
 // Truco para usar siempre un puerto vacío, usando el puerto 0
 // Ojo usar en desarrollo
