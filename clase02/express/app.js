7ac8f1c7531893734e2d54e92d6034717212c681
DeiMoS <ndelgado@hiberus.com>
2023-08-03 18:22:02 +0200

    parametros query

--- clase02/express/app.js
+++ clase02/express/app.js
@@ -23,6 +23,19 @@ app.get('/api/cursos/programacion/:lenguaje', (req, res) => {
     if(resultados.length === 0){
         return res.status(404).send(`No se encontraron curso de: ${lenguaje}.`);
     }
+    if(req.query.ordenar === 'vistas'){
+       return res.send(JSON.stringify(resultados.sort((a, b) => b.vistas - a.vistas)));
+    } 
+    
+    res.send(JSON.stringify(resultados));
+})
+app.get('/api/cursos/programacion/:lenguaje/:nivel', (req, res) => {
+    const lenguaje = req.params.lenguaje;
+    const nivel = req.params.nivel;
+    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel);
+    if(resultados.length === 0){
+        return res.status(404).send(`No se encontraron cursos de: ${lenguaje} de nivel ${nivel}.`);
+    }
     res.send(JSON.stringify(resultados));
 })
 
@@ -38,16 +51,6 @@ app.get('/api/cursos/matematicas/:tema', (req, res) => {
     }
     res.send(JSON.stringify(resultados));
 })
-app.get('/api/cursos/programacion/:lenguaje/:nivel', (req, res) => {
-    const lenguaje = req.params.lenguaje;
-    const nivel = req.params.nivel;
-    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel);
-    if(resultados.length === 0){
-        return res.status(404).send(`No se encontraron cursos de: ${lenguaje} de nivel ${nivel}.`);
-    }
-    res.send(JSON.stringify(resultados));
-})
-
 
 const PUERTO = process.env.PORT || 3000;
 
