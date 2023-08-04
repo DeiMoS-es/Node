1879f031459e13ae85740ba29e94e59dd28470ed
DeiMoS <ndelgado@hiberus.com>
2023-08-04 11:24:01 +0200

    post, put

--- clase02/express/routers/programacion.js
+++ clase02/express/routers/programacion.js
@@ -2,10 +2,12 @@ const express = require('express');
 const routerProgramacion = express.Router();
 const { programacion } = require('../datos/cursos.js').infoCursos;
 
+// Middelware
+routerProgramacion.use(express.json()); // nos permite procesar el cuerpo de la solicitud en formato JSON
 // Cursos Programacion
 routerProgramacion.get('/', (req, res) => {
     res.send(JSON.stringify(programacion));
-})
+});
 routerProgramacion.get('/:lenguaje', (req, res) => {
     const lenguaje = req.params.lenguaje;
     const resultados = programacion.filter(curso => curso.lenguaje === lenguaje);
@@ -17,7 +19,7 @@ routerProgramacion.get('/:lenguaje', (req, res) => {
     } 
 
     res.send(JSON.stringify(resultados));
-})
+});
 routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
     const lenguaje = req.params.lenguaje;
     const nivel = req.params.nivel;
@@ -26,6 +28,23 @@ routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
         return res.status(404).send(`No se encontraron cursos de: ${lenguaje} de nivel ${nivel}.`);
     }
     res.send(JSON.stringify(resultados));
-})
+});
+routerProgramacion.post('/', (req, res) => {
+    // Para extraer el cuerpo de la solicitud para poder incluir un curso nuevo
+    let cursoNuevo = req.body;
+    programacion.push(cursoNuevo);
+    res.send(JSON.stringify(programacion));
+});
+routerProgramacion.put('/:id', (req, res) => {
+    const cursoActualizado = req.body;
+    const id = req.params.id;
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if (indice >= 0){
+        programacion[indice] = cursoActualizado;
+    }
+    res.send(JSON.stringify(programacion));
+
+});
 
 module.exports = routerProgramacion;
