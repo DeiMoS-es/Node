202c2a4db3d1ba5fed1c45919e3dcd45ff17c6c0
DeiMoS <ndelgado@hiberus.com>
2024-01-12 10:11:29 +0100

    terminada ruta dinámica

--- AppTarifaLuzBack/routes/api/precios.js
+++ AppTarifaLuzBack/routes/api/precios.js
@@ -1,7 +1,14 @@
 // Encargado de gestionar las peticiones para obtener los precios de las distintas CCAA
 const router = require('express').Router();
 
+router.get('/', (req, res) => {// Petición sobre la ruta /api/precios
+
+});
 
 
+/*
+Ejemplo para cuando añada la opción de seleccionar la zona, zona será un parámetro que nos viene por url
+router.get('/:zone', (req, res) => {});
+*/
 
 module.exports = router;
