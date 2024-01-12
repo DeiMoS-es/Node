18f0fdd95c7b7b6d711686cbc804083fdc72794e
DeiMoS <ndelgado@hiberus.com>
2024-01-12 14:04:52 +0100

    preparado solicitudes por zon

--- AppTarifaLuzBack/models/electricidad.precios.js
+++ AppTarifaLuzBack/models/electricidad.precios.js
@@ -1,27 +1,23 @@
 // Este será mi modelo para obtener los precios
 class precioModel {
     constructor(response) {
-        this.dataType = response.data.type;
-
+        this.precioZona = response.data.type;
         // Comprueba si 'included' existe y tiene al menos un elemento
         if (response.included && response.included.length > 0) {
-            // Puedes ajustar esta parte según tus necesidades específicas
-            this.includeTypes = response.included.map(item => item.type);
-            this.values = response.included.flatMap(item => item.attributes.values.map(valueItem => new Value(valueItem)));
+            this.moneda = response.included.map(item => item.type);
+            this.preciosHoras = response.included.flatMap(item => item.attributes.values.map(valueItem => new Value(valueItem)));
         } else {
-            this.includeTypes = [];
-            this.values = [];
+            this.moneda = [];
+            this.preciosHoras = [];
         }
     }
 }
 
-// Resto de la clase Value
 class Value {
     constructor(item) {
-        this.value = item.value;
+        this.precio = item.value;
         this.datetime = item.datetime;
     }
 }
-
 
 module.exports = precioModel;
