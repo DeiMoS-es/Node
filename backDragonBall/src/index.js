7a5016dfed7eb022b0c7bc4e87a6a381456d901b
DeiMoS <ndelgado@hiberus.com>
2024-08-26 17:09:00 +0200

    chore: Update server configuration and routes in vercel.json

--- backDragonBall/src/index.js
+++ backDragonBall/src/index.js
@@ -1,4 +1,4 @@
-// Primer paso, configurar express
+ // Primer paso, configurar express
 const express = require('express'); // Traemos la libreria
 const http = require('http');
 const socketIo = require('socket.io');
@@ -19,8 +19,22 @@ app.use(express.urlencoded({ extended: false}));
 // Pensar en las rutas que vamos a usar para la llamada
 // Delegamos la peticiones que vayan por la ruta /api al fichero api.js
 app.use('/api', require('./routes/api'));
+app.get("/", (req, res) => {
+  const htmlResponse = `
+  <html> 
+    <head>
+      <title>API REST</title>
+    </head>
+    <body>
+      <h1>API REST</h1>
+      <p>Esta es la API REST de la aplicación de chat</p>
+    </body> 
+  </html>
+    `;
+    res.send(htmlResponse);
+}
+);
 
-// Ponemos la aplicación a escuchar
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en puerto ${PORT}`);
